3706d25f74d5717afd88839c7333c84e333562bb
ch261 <1670501444@qq.com>
2019-10-28 09:12:04 +0800

    客户端接入投票页面

--- app/src/main/java/com/example/myapp/Frame.java
+++ app/src/main/java/com/example/myapp/Frame.java
@@ -7,7 +7,6 @@ import android.widget.RadioGroup;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.ListFragment;
 
 public class Frame extends FragmentActivity {
 
@@ -21,14 +20,6 @@ public class Frame extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        mFrangments=new Frame[3];
-        fragmentManager=getSupportFragmentManager();
-        mFrangments[0]=(ListFragment)getFragmentManager().findFragmentById(R.id.fragment_main);
-        mFrangments[1]=fragmentManager.findFragmentById(R.id.fragment_main);
-        mFrangments[2]=fragmentManager.findFragmentById(R.id.fragment_main);
-        fragmentTransaction=
-                fragmentManager.beginTransaction().hide(mFrangments[0].hide(mFrangments[1].hide(mFrangments[2])));
-        fragmentTransaction.show(mFrangments[0].commit);
-        radioGroup=findViewById(R.id.bottomgroup);
+
     }
 }
